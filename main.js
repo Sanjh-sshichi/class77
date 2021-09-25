1d399146ad94fce046ac944fda7f923d5fda9aea
Sanjh-sshichi <83003985+Sanjh-sshichi@users.noreply.github.com>
2021-09-25 13:02:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var no1=15;
+var square_root=Math.sqrt(16);
+console.log(square_root);
+var fname="Sanjh";
+var lname="Brahmachary";
+console.log(fname+" "+lname);
+var length=fname.length;
+console.log(length);
